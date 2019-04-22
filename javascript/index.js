798a6344e4cd24ab75154152c1f33ce7e740878d
Adriana M <adrianaymontilla@gmail.com>
2019-04-22 14:15:06 -0300

    finish

--- javascript/index.js
+++ javascript/index.js
@@ -1,31 +1,26 @@
-window.addEventListener("keydown",function(e){
-    console.log(e);
-    
-})
+
 
 function removeTransition(e) {
-    console.log(e)
     if (e.propertyName !== "transform") return;
     e.target.classList.remove("playing")
   
-     console.log(this);
-    
-}
+     
+   }
+
 function playsound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if (!audio) return;
-
     audio.currentTime = 0;
     audio.play()
-    
-    setTimeout(() => {
-        key.classList.add("playing");   
-    }, 1.0);
+    key.classList.add("playing");   
+  
+  
     
 }
 
 
-const keys = Array.from(document.querySelectorAll(".key")) ;
-keys.forEach(key => key.addEventListener("transitioned", removeTransition))
+const keys = document.querySelectorAll(".key") ; 
+keys.forEach(key => key.addEventListener("transitionend", removeTransition))
 window.addEventListener("keydown", playsound);
+ 
